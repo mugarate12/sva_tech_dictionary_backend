cec47f1dc0affeda7c1fdec316691cf145c7b9a3
Mateus Cardoso <serjumano17@gmail.com>
2022-09-20 23:13:33 -0300

    :fire: refactor: remove unnecessary files

--- src/services/Example.services.ts
+++
@@ -1,6 +0,0 @@
-export default class ExampleService {
-  public formatExampleMessage (name?: string) {
-    name = !!name && name !== 'undefined' ? name : '';
-    return name ? `Bem vindo, ${name}! API está funcionando!` : 'API está funcionando!';
-  }
-}
